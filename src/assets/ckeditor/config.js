855950494b624cb3554c86445eb5ef42afbdaeef
Vadim Mazur <mazurva@gmail.com>
2016-01-04 17:56:17 +0600

    Update config & CKEditor.php

--- src/assets/ckeditor/config.js
+++ src/assets/ckeditor/config.js
@@ -7,7 +7,8 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
-    config.extraPlugins = 'fontawesome,autocorrect,widgetbootstrap,widgettemplatemenu';
+    config.extraPlugins = 'fontawesome,autocorrect,widgetbootstrap,widgettemplatemenu,ckeditor-gwf-plugin';
     config.contentsCss = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css';
     config.allowedContent = true;
+    config.font_names = 'GoogleWebFonts';
 };
