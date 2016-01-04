30bcd64f8bdb77df5595e4824b39af8247a3f20a
Vadim Mazur <mazurva@gmail.com>
2016-01-04 17:23:30 +0600

    Update CKEditor

--- src/assets/ckeditor/config.js
+++ src/assets/ckeditor/config.js
@@ -7,4 +7,7 @@ CKEDITOR.editorConfig = function( config ) {
 	// Define changes to default configuration here. For example:
 	// config.language = 'fr';
 	// config.uiColor = '#AADC6E';
+    config.extraPlugins = 'fontawesome,autocorrect,widgetbootstrap,widgettemplatemenu';
+    config.contentsCss = 'https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css';
+    config.allowedContent = true;
 };
